bd978197082e7a7d08b12879450fed82ce8bb719
ewhitebloom <ewhitebloom@gmail.com>
2014-02-21 11:19:11 -0500

    First commit

---
+++ game.rb
@@ -0,0 +1,20 @@
+NUMBER = 673
+
+message = print "Guess a number between 0 and 1000:"
+guess = gets.chomp.to_i
+
+until guess == NUMBER
+  message = "Guess a number between 0 and 1000:"
+  difference = NUMBER - guess
+  if difference >= 0
+    puts "Too low, try again..."
+    print message
+    guess = gets.chomp.to_i
+  elsif difference <= 0
+    puts "Too high, try again..."
+    print message
+    guess = gets.chomp.to_i
+  end
+end
+
+puts "Congratulations, you've guessed the number"
